96660945f5b9ddbb69b5417bc4d652b74dbd6ae8
JuanSebastianCondeFarias <juansebastian.condefarias@gmail.com>
2022-06-14 20:13:38 -0500

    Relaciones en modelos

--- prueba_laravel/app/Models/Employee.php
+++ prueba_laravel/app/Models/Employee.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function contracts()
+    {
+        return $this->hasMany(Contract::class, 'employees_id');
+    }
+    public function childrens()
+    {
+        return $this->hasMany(Children::class, 'employees_id');
+    }
 }
--- prueba_laravel/app/Models/Type.php
+++ prueba_laravel/app/Models/Type.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+    public function employees()
+    {
+        return $this->hasMany(Employee::class, 'types_id');
+    }
 }
